bff3694c5b161baa7f9793e95ecffaab975f34cc
sindhulandfox <sindhu.tmg@gmail.com>
2022-09-03 15:11:51 +0530

    helo

---
+++ src/gitproject/Simple.java
@@ -0,0 +1,26 @@
+package gitproject;
+
+
+
+	public class Simple {
+		
+		public void empId() {
+			System.out.println("enter employee id");
+
+		}
+		public void empname() {
+			System.out.println("enter employee name :+name");
+			
+
+		}
+		public static void main(String [] args) {
+			Simple e = new Simple();
+			e.empId();
+			e.empname();
+			
+		}
+
+	}
+
+
+}
